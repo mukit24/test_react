04cba304667fe697e1ff1b3901e2b357073f2309
mukit hasan pranto <mukithasanpranto4@gmail.com>
2020-05-27 17:19:28 +0600

    Updates

---
+++ precache-manifest.cfc81cde53019af003c1f09efe98450a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa700444ef3318758cba8029caccde90",
+    "url": "/test_react/index.html"
+  },
+  {
+    "revision": "47a9f26cc05ac527247b",
+    "url": "/test_react/static/css/main.f57aa281.chunk.css"
+  },
+  {
+    "revision": "319b774e7719e54e9fef",
+    "url": "/test_react/static/js/2.592238d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test_react/static/js/2.592238d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47a9f26cc05ac527247b",
+    "url": "/test_react/static/js/main.97685366.chunk.js"
+  },
+  {
+    "revision": "d9c24836d1c133344382",
+    "url": "/test_react/static/js/runtime-main.d0a46e37.js"
+  }
+]);
